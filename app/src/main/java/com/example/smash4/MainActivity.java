777857e45a1e89ade8449b4983a52435080b3df3
yuko-ye <yuko.ye369@gmail.com>
2021-04-18 15:29:37 +0900

    電卓開いた

--- app/src/main/java/com/example/smash4/MainActivity.java
+++ app/src/main/java/com/example/smash4/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -55,7 +57,35 @@ private Button shareButton;
 
 
 
-    });}}
+    });}
+
+
+
+    public void onButton (View view){
+        //Editの取得
+        EditText eT1 = (EditText)findViewById(R.id.eT1);
+        EditText eT2 = (EditText)findViewById(R.id.eT2);
+
+        //Edit⇒String
+        String str1 = eT1.getText().toString();
+        String str2 = eT2.getText().toString();
+
+        //数値に変換
+        int num1= Integer.parseInt(str1);
+        int num2= Integer.parseInt(str2);
+
+        //足し算
+        int sum = num1 + num2;
+        ((TextView)findViewById(R.id.tvAns)).setText("" + sum);
+    }
+
+
+
+    public void onClear (View view){
+        ((EditText)findViewById(R.id.eT1)).getText().clear();
+        ((EditText)findViewById(R.id.eT2)).getText().clear();
+        ((TextView)findViewById(R.id.tvAns)).setText("Answer");
+    }}
 
 
 
--- app/src/main/java/com/example/smash4/ui/menu/MenuFragment.java
+++ app/src/main/java/com/example/smash4/ui/menu/MenuFragment.java
@@ -28,39 +28,13 @@ public class MenuFragment extends Fragment {
            // public void onChanged(@Nullable String s) {
              //   textView.setText(s);
 
-
-
         return root;
+
 
 
 
     }
 
-
-
-            public void onButton(View view){
-            //Editの取得
-                  EditText eT1 = (EditText)view.findViewById(R.id.eT1);
-                EditText eT2 = (EditText)view.findViewById(R.id.eT2);
-
-                      //Edit⇒String
-                    String str1 = eT1.getText().toString();
-                  String str2 = eT2.getText().toString();
-
-                      //数値に変換
-                    int num1= Integer.parseInt(str1);
-                  int num2= Integer.parseInt(str2);
-
-                      //足し算
-                    int sum = num1 + num2;
-                  ((TextView)view.findViewById(R.id.tvAns)).setText("a" + sum);
-            }
-
-                  public void onClear(View view){
-                    ((EditText)view.findViewById(R.id.eT1)).getText().clear();
-                  ((EditText)view.findViewById(R.id.eT2)).getText().clear();
-                ((TextView)view.findViewById(R.id.tvAns)).setText("Answer");
-                   }
 
 
 }
